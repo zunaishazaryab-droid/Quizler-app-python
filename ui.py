f6447c17efe83df8444be862185779b1388daafa
Arslan Yousaf <devtechofficial90@gmail.com>
2025-05-12 10:04:27 +0500

    Implement answer handling and feedback in QuizInterface; update UI for question navigation

--- ui.py
+++ ui.py
@@ -14,19 +14,56 @@ class QuizInterface:
         self.question_text = self.canvas.create_text(150, 125, width=280, text="Some Question text", fill=THEME_COLOR, font=("arial", 20 , "italic"))
         self.canvas.grid(row=1, column=0,columnspan=2, pady=50)
         true_image = PhotoImage(file="images/true.png")
-        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button = Button(image=true_image, highlightthickness=0, command=self.is_answer_true)
         self.true_button.grid(row=2, column=0)
         false_image = PhotoImage(file="images/false.png")
-        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button = Button(image=false_image, highlightthickness=0, command=self.is_answer_false)
         self.false_button.grid(row=2, column=1)
         
         self.get_next_question()
         
         
         
+        
         self.window.mainloop()
         
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text= q_text)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.canvas.config(bg="white")
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            self.canvas.itemconfig(self.question_text, text= q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text= "You reached the End of quiz")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+    
+    def is_answer_false(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+    
+    def is_answer_true(self):
+        is_right = self.quiz.check_answer("True")
+        self.give_feedback(is_right)
+    
+    def give_feedback(self, is_right:bool):
+        if is_right:
+            self.green_color()
+            self.window.after(1000, self.get_next_question)
+        else:
+            self.red_color()
+            self.window.after(1000, self.get_next_question)
+           
+            
+            
+    
+    def green_color(self):
+        self.canvas.config(bg="Green")
+    def red_color(self):
+        self.canvas.config(bg="Red")
+        
+        
+        
+        
             
